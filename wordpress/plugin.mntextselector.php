a06fc05d20ab2143c9704c47840b8a96524ce60f
Tim Garrison <tim@telligense.com>
2013-03-27 12:11:45 -0700

    - intial plugin file, doesn't do anyting yet

---
+++ wordpress/plugin.mntextselector.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: mnTextSelector jQuery plugin
+Plugin URI: https://github.com/mitnosirrag/mnTextSelector
+Description: Quickly add callback functions to use with user-highlighted text
+Author: Tim Garrison
+Version 1.0
+Author URI: http://mitnosirrag.tumblr.com
+*/
+?>
